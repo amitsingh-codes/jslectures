f69eb24c0bc75aecad5041171fa74c29d775c18f
Amit singh <amitwashere9@gmail.com>
2025-08-21 21:00:50 +0530

    lectures

---
+++ D&C.js
@@ -0,0 +1,202 @@
+console.log("hello 'amit here'");
+
+console.log('hello "Amit here"');
+
+console.log(`"Hello" 'Amit here'`);
+
+let name = "Amit";
+console.log("My name is", name)
+console.log(`My name is ${name}`) //better 
+
+
+console.log( `the result is ${1 + 2}`);
+
+console.log(typeof(name))
+
+
+const newline = `One day you finally knew
+what you had to do, and began,`;
+console.log(newline);
+
+const name1 = "Front ";
+const number = 242;
+console.log(name1 + number)
+console.log(typeof(name1 + number)); // "Front 242"
+
+console.log("O">"O");
+console.log("O">"o");
+console.log(56<100);
+
+console.log("5"==5) //string "5" became number 5
+console.log( 1 || 0 );
+console.log( null || 1 );
+console.log( null || 0 || 1 );
+console.log( undefined || null || 0 );
+
+console.log(true && true )
+console.log(0 && 0);
+console.log(1 && 1);
+
+let a1 = "car"
+if (a1 == "car"){
+    console.log("it is ")
+}
+else {
+    console.log("it is not")
+}
+
+let shoppingDone = false;
+let childAllowance ;
+console.log(childAllowance)
+if (shoppingDone === true) {
+  childAllowance = 10;
+} else {
+  childAllowance = 5;
+}
+console.log(childAllowance)
+
+
+const select = document.querySelector("select");
+const para = document.querySelector("p");
+
+select.addEventListener("change", setWeather);
+
+function setWeather() {
+  const choice = select.value;
+
+  if (choice === "sunny") {
+    para.textContent =
+      "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+  }
+   else if (choice === "rainy") {
+    para.textContent =
+      "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long.";
+  } 
+  else if (choice === "snowing") {
+    para.textContent =
+      "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
+  } 
+  else if (choice === "overcast") {
+    para.textContent =
+      "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
+  }
+   else {
+    para.textContent = "";
+  }
+}
+
+let cheese = false;
+
+if (cheese) {
+  console.log("Yay! Cheese available for making cheese on toast.");
+} 
+else {
+  console.log("No cheese on toast for you today.");
+}
+
+let age = 160;
+
+if (age<15){
+    console.log("Not allowed ")
+}
+else if (age>15 && age <18){
+    console.log("allowed with learning permit")
+}
+else if (age>50 && age==18 && age>18){
+    console.log("allowed")
+}
+else{
+    console.log("not allowed due to high age")
+}
+
+let n = prompt(`What's the “official” name of JavaScript?`);
+if (n == "ECMAScript"){
+  alert("Right")
+} 
+else{
+  alert(`You don't know? “ECMAScript”!`)
+}
+
+let num = prompt(`Enter a random number`);
+
+switch (num){
+  case (num > 0):
+    alert("1")
+    break;
+  case (num < 0):
+    alert("-1")
+    break
+  default:
+    alert("0")
+}
+
+//it won't work cause switch statement doesn't work on conditions it work when we give a variable a name or value 
+
+
+let message1 = (num > 0) ? "1" : 
+    (num<0) ? "-1":
+    "0"
+alert(message1)
+
+let a2 = prompt("Enter a")
+let b1 = prompt("Enter b")
+
+let result;
+if (a2 + b1 < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+let a3 = Number(prompt("Enter a"));
+let b = Number(prompt("Enter b"));
+let result1 = (a3 + b < 4 ) ?"Below" : "over"
+alert(result1)
+
+let message2;
+if (login == 'Employee') {
+  message2 = 'Hello';
+} else if (login == 'Director') {
+  message2 = 'Greetings';
+} else if (login == '') {
+  message2 = 'No login';
+} else {
+  message2 = '';
+}
+
+let login = prompt("enter your designation")
+
+let message = (login == "Employee") ? "Hello":
+    (login=="Director") ? "Greetings" :
+    (login == "") ? "No login":
+    "";
+alert(message)
+
+let browser = prompt("Enter your browser")
+
+if (browser == "Edge"){
+  alert("You've got the Edge!")
+}
+
+else if (browser == "chrome"||"Firefox"||"Safari"||"Opera"){
+  alert("Okay we support this browser too")
+}
+
+else{
+  alert( 'We hope that this page looks ok!' )
+}
+
+let a = +prompt('a?')
+
+switch(a){
+  case 0 :
+    alert("0")
+    break;
+  case 1 :
+    alert(1)
+    break;
+  case 2:
+  case 3:
+    alert('2,3')
+    break;
+}
---
+++ V&O.js
@@ -0,0 +1,40 @@
+console.log("Hello! world");
+
+let name =  "Amit";
+let surname = "Singh";
+
+console.log(name , surname);
+console.log(surname);
+console.log(name);
+
+let age = 11;
+console.log(age);
+
+age = 52;
+console.log(age);
+
+const pi = 3.14;
+// pi = 10;
+
+console.log(pi);
+
+//Assignment
+console.log(35+90);
+
+console.log(35+60+92+98+59+56);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a)
+a = 50;
+console.log(a)
+let b = 7*a
+console.log(b)
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual/max ;
+console.log(percentage)
+
+console.log(1+3*4/2*3)
