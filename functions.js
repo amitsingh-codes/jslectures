9a8863908c6cb5822c09784dc4dc1535282c8a9e
Amit singh <amitwashere9@gmail.com>
2025-08-22 22:05:30 +0530

    learning functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!"
+}
+
+console.log(favoriteAnimal('Goat'))
+
+
+const myArray = ["I", "love", "chocolate", "frogs"];
+const madeAString = myArray.join(" ");
+console.log(madeAString);
+// the join() function takes an array, joins
+// all the array items together into a single
+// string, and returns this new string
