799f96316980b66207f1a8fc794897f6eb7ccfcb
Amit singh <amitwashere9@gmail.com>
2025-08-23 18:25:13 +0530

    functions

--- functions.js
+++ functions.js
@@ -11,3 +11,19 @@ console.log(madeAString);
 // the join() function takes an array, joins
 // all the array items together into a single
 // string, and returns this new string
+
+
+// (function () {
+//     alert("Hello");
+// })
+//     ();
+
+// textBox.addEventListener("keydown", function (event) {
+//   console.log(`You pressed "${event.key}".`);
+// });
+
+const originals = [1, 2, 3];
+
+const doubled = originals.map(item => item * 2);
+
+console.log(doubled); // [2, 4, 6]
