b612c75dd94dcb5dbbc74be681a922fbfb1dd156
Amit singh <amitwashere9@gmail.com>
2025-08-25 01:56:32 +0530

    resolved merge conflicts

--- functions.js
+++ functions.js
@@ -37,34 +37,3 @@ textBox.addEventListener("keydown", (event) => {
 });
 
 const textbox = document.querySelector("#textBox");
-=======
-function favoriteAnimal(animal) {
-    return animal + " is my favorite animal!"
-}
-
-console.log(favoriteAnimal('Goat'))
-
-
-const myArray = ["I", "love", "chocolate", "frogs"];
-const madeAString = myArray.join(" ");
-console.log(madeAString);
-// the join() function takes an array, joins
-// all the array items together into a single
-// string, and returns this new string
-
-
-// (function () {
-//     alert("Hello");
-// })
-//     ();
-
-// textBox.addEventListener("keydown", function (event) {
-//   console.log(`You pressed "${event.key}".`);
-// });
-
-const originals = [1, 2, 3];
-
-const doubled = originals.map(item => item * 2);
-
-console.log(doubled); // [2, 4, 6]
->>>>>>> cb0601fd284bda19f33fa3c63ed95e5b4c64beb2
