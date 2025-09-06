44f2c502d96d126db7fbffe3b54ba567ecaaf66a
Amit singh <amitwashere9@gmail.com>
2025-09-06 12:38:29 +0530

    did a fizz buzz question

--- problemsolving.js
+++ problemsolving.js
@@ -1,1 +1,16 @@
 let num = Number(prompt("Enter the number"))
+
+for (let i = 1; i <= num; i++){
+    if (i%3 == 0 && i%5 == 0){
+        console.log("Fizz Buzz")
+    }
+    else if (i%3 == 0 && i%5 != 0 ){
+        console.log("fizz")
+    }   
+    else if (i%3 != 0 && i%5 == 0){
+        console.log("Buzz")
+    }
+    else{
+        console.log(i)
+    }
+}
