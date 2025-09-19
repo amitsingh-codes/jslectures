71f1808a89b0901a3d02215d261b74bd7c6231af
Amit singh <amitwashere9@gmail.com>
2025-09-19 11:09:36 +0530

    new file added

---
+++ loops&array.js
@@ -0,0 +1,104 @@
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+for (const cat of cats) {
+  console.log(cat);
+}
+
+let Array = ["car","bike"]
+for (let index = 0; index < Array.length; index++) {
+    const element = Array[index];
+    console.log(element)
+}
+
+let myFavoriteCats = "My cats are called ";
+
+for (let i = 0; i < cats.length; i++) {
+  if (i === cats.length - 1) {
+    // We are at the end of the array
+    myFavoriteCats += `and ${cats[i]}.`;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+}
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
+
+
+const contacts = [
+  "Chris:2232322",
+  "Sarah:3453456",
+  "Bill:7654322",
+  "Mary:9998769",
+  "Dianne:9384975",
+];
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  const searchName = input.value.toLowerCase();
+  input.value = "";
+  input.focus();
+  para.textContent = "";
+  for (const contact of contacts) {
+    const splitContact = contact.split(":");
+    if (splitContact[0].toLowerCase() === searchName) {
+      para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+      break;
+    }
+  }
+  if (para.textContent === "") {
+    para.textContent = "Contact not found.";
+  }
+});
+
+// const cats = ["Pete", "Biggles", "Jasmine"];
+
+// let myFavoriteCats = "My cats are called ";
+
+let i = 0;
+
+while (i < cats.length) {
+  if (i === cats.length - 1) {
+    myFavoriteCats += `and ${cats[i]}.`;
+  } 
+  else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+
+  i++;
+}
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
+
+do {
+  if (i === cats.length - 1) {
+    myFavoriteCats += `and ${cats[i]}.`;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+
+  i++;
+} while (i < cats.length);
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
+
+let i1 = 10;
+
+while(i1>=0){
+  if (i1==10){
+    console.log("Countdown",i1)
+  }
+  else if (i1==0){
+    console.log("Blast off!")
+  }
+  else {
+    console.log(i1)
+  }
+  i1--
+}
+
+let a= prompt("Enter a number greater than 100") 
+while (a<100){
+a = prompt("The number should be greater then 100!!!!!!!!")
+}   
