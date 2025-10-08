292f3b4e8369e9ebfef53714efc91cd6bcf69169
Amit singh <amitwashere9@gmail.com>
2025-10-08 21:44:22 +0530

    adding HTML content

--- DOM.js
+++ DOM.js
@@ -19,5 +19,33 @@ console.log(container)
 controls.style.background = "blue";
 // adds several style rules
 controls.style.cssText = "color:red; background-color:green;width:200px ;"
-// adds several style rules
+// adds several style rules without CssText
 controls.setAttribute("style","color:green; background:black;width:150px")
+//we can use  camelcase or even kebab-case here
+controls.style["backgroundColor"]="red"
+
+// if id exists, update it to 'theDiv', else create an id with value "theDiv"
+div.setAttribute("id", "theDiv");
+console.log(div)
+
+// returns value of specified attribute, in this case "theDiv"
+console.log(div.getAttribute("id"));
+
+// removes specified attribute
+div.removeAttribute("id");
+console.log(div)
+
+//adds class "new" to div element
+div.classList.add("New")
+
+//removes class "new" from div element
+div.classList.remove("New")
+
+//if div have class "active" removes it else adds it 
+div.classList.toggle("active")
+
+//Adding text content
+div.textContent = "Hello World!"    //<div>Hello World!</div>
+
+//adding HTML content 
+div.innerHTML = "<span>text</span>"
